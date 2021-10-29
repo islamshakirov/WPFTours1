f0e770064add1dcc5695ff81b4c376165c43498e
orpmis <71081234+orpmis@users.noreply.github.com>
2021-10-30 00:14:22 +0300

    1

---
+++ demo/demo/Hotelspage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace demo
+{
+    /// <summary>
+    /// Логика взаимодействия для Hotelspage.xaml
+    /// </summary>
+    public partial class Hotelspage : Page
+    {
+        public Hotelspage()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainWindow.Navigate(new AddEditPage());
+        }
+
+        private void Redacthotel1_Click(object sender, RoutedEventArgs e)
+        {
+            new AddRedactHotelWindow().Show();
+        }
+
+        private void Redacthotel2_Click(object sender, RoutedEventArgs e)
+        {
+            new AddRedactHotelWindow().Show();
+        }
+
+        private void Addhotel_Click(object sender, RoutedEventArgs e)
+        {
+            new AddRedactHotelWindow().Show();
+        }
+    }
+}
---
+++ demo/demo/Main.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace demo
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            MainWindow.Navigate(new Tourspage());
+            Manager.MainWindow = MainWindow;
+        }
+
+        private void backbtn_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainWindow.GoBack();
+        }
+
+        private void MainWindow_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainWindow.CanGoBack)
+            {
+                backbtn.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                backbtn.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainWindow.Navigate(new Hotelspage());
+        }
+    }
+}
